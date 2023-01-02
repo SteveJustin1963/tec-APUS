97cc9fe52123f2e74f49625a26fd0a32d8c04780
Stephen Justin <stevejustin1963@gmail.com>
2023-01-02 13:59:14 +1100

    Update 9511.f

--- 9511 test/9511.f
+++ 9511 test/9511.f
@@ -26,35 +26,4 @@
    RESULT !
 ;
 
-$800 start
-=====================
-
-: pushData ( n -- )
-   DATA_PORT c@ swap
-   DATA_PORT c!
-;
-
-: popData ( -- n )
-   DATA_PORT c@
-   DATA_PORT c@
-;
-
-: awaitResult ( -- )
-   BEGIN
-      STATUS_PORT c@ AND BUSY
-   UNTIL
-   0=
-;
-
-: start ( -- )
-   1 DE! 1 HL!
-   pushData
-   DE HL!
-   pushData
-   SADD COMMAND_PORT c!
-   awaitResult
-   popData
-   RESULT !
-;
-
-$800 start
+$800 start 
